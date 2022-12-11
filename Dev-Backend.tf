783cf521be0501553be1de7fcc31e184fe33adfc
Sravani Palukuri <sravanipalukuru25@gmail.com>
2022-12-11 23:05:29 +0530

    Workspaces

--- Dev-Backend.tf
+++ Dev-Backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "devopsterraformstate"
-    key            = "dev-devopsb30.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "devopsb31-dynamodb-locking"
-    encrypt        = true
+    bucket = "devopsterraformstate"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    # dynamodb_table = "devopsb31-dynamodb-locking"
+    # encrypt        = true
   }
 }   
--- UAT.tfvars
+++ UAT.tfvars
@@ -1,6 +1,6 @@
 aws_region          = "us-east-1"
-environment         = "dev"
-vpc_name            = "devopsb31-vpc"
+environment         = "uat"
+vpc_name            = "devopsb31-uat-vpc"
 vpc_cidr            = "10.1.0.0/16"
 public_subnet1_cidr = "10.1.1.0/24"
 public_subnet2_cidr = "10.1.2.0/24"
